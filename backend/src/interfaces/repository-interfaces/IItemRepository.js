f715d996385b3b3ede5d67cfedfe75b5dc6dd792
ksb3 <ksb3@g3c19.cin.ufpe.br>
2026-05-06 19:25:52 -0300

    feat(IItemRepository.js): adiciona interface do ItemRepository

---
+++ backend/src/interfaces/repository-interfaces/IItemRepository.js
@@ -0,0 +1,20 @@
+export class IItemRepository {
+    createItem(name, quantity){
+        throw new Error("not implemented")
+    }
+    getAllItems() {
+        throw new Error("not implemented")
+    }
+    getItemByID(id){
+        throw new Error("not implemented")
+    }
+    updateQuantity(id, newQuantity){
+        throw new Error("not implemented")
+    }
+    updateName(id, newName){
+        throw new Error("not implemented")
+    }
+    deleteItem(id){
+        throw new Error("not implemented")
+    }
+}
