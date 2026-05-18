101252a68260c8565716750f43fefc5ee95a3b27
joseromildo0 <jrdsj@cin.ufpe.br>
2026-05-18 17:32:24 -0300

    feat(change item name): Edição de nome de item
    Adição da função de editar o nome de item e instalação do vitest .

--- projeto/backend/src/interfaces/service-interfaces/IItemService.ts
+++ projeto/backend/src/interfaces/service-interfaces/IItemService.ts
@@ -2,4 +2,5 @@ import type { Item } from "../../entities/Item.js";
 
 export interface IItemService{
     addStock(id: string, quantity: number): Promise<Item>
+    changeItemName(id: string, name: string): Promise<Item>
 }
