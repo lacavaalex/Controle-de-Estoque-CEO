4b199304d5785c8663f7c5feeb3e0f20b6810978
Alex Lacava Jr <alexlacavajr@gmail.com>
2026-05-20 14:19:47 -0300

    feat(backend): cria arquivos de autenticação e registro de usuário
    Aplicando regras de negócio, foram criados os arquivos de login e cadastro de usuário, seguindo o modelo de Arquitetura em Camadas.

--- projeto/backend/src/di/container.ts
+++ projeto/backend/src/di/container.ts
@@ -2,8 +2,16 @@ import { JsonItemRepo } from "../repositories/JsonItemRepo.js";
 import { ItemService } from "../services/ItemService.js";
 import { ItemController } from "../controller/ItemController.js";
 
+import { JsonUserRepo } from "../repositories/JsonUserRepo.js";
+import { UserService } from "../services/UserService.js";
+import { UserController } from "../controller/UserController.js";
+
 const itemRepo = new JsonItemRepo();
 const itemService = new ItemService(itemRepo);
 const itemController = new ItemController(itemService);
 
-export {itemController}
+const userRepo = new JsonUserRepo();
+const userService = new UserService(userRepo);
+const userController = new UserController(userService);
+
+export { itemController, userController };
